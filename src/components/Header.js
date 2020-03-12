ed09d5a93e6152a76e2881713a0d7e9ccfe8df71
alexlarkwo <squirtle68@gmail.com>
2020-03-13 12:45:33 +1300

    first commit of first react test

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <nav>
+         <a className="pageOne" href=""> Page 1 </a>
+         <a className="pageTwo" href=""> Page 2 </a>
+         <a className="pageThree" href=""> Page 3 </a>
+         <a className="logo" href=""> LOGO </a>
+        </nav>
+    )
+}
+ 
